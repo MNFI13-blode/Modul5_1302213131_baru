699a7a4b50bdc1958fa455e1f02d83c9b4ec329d
PC5-IFLAB <sylvana.freelance@gmail.com>
2023-03-24 09:07:46 +0700

    Jurmod5_1302213131

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    public class penjumlahan
+    {
+        public static void Main(string[] args)
+        {
+            penjumlahan.JumlahTigaAngka<float>(13, 45, 67);
+        }
+        static void JumlahTigaAngka<T>(T nomor, T nomor2, T nomor3)
+        {
+            Console.WriteLine((dynamic)nomor+(dynamic)nomor2+(dynamic)nomor3);
+        }
+    }
+}
